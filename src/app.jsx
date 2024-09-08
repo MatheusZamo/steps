8fd32ae3e0530f655b57cd6971872bfb009914ae
MatheusZamo <19matheushenrique03@gmail.com>
2024-09-07 22:19:17 -0300

    Initial code

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,42 @@
-const App = () => <h1>Boilerplate Vite + React</h1>
+const App = () => (
+  <>
+    <div className="container-close">
+      <button
+        className="close"
+        onClick={(e) => console.log(e.target.textContent)}
+      >
+        Fechar
+      </button>
+    </div>
+
+    <div className="steps">
+      <div className="numbers">
+        <div className="active">
+          <span>1</span>
+        </div>
+        <div>
+          <span>2</span>
+        </div>
+        <div>
+          <span>3</span>
+        </div>
+      </div>
+      <h2 className="message">Passo 1: Entender o problema do cliente</h2>
+
+      <div className="buttons">
+        <button>
+          <span onClick={(e) => console.log(e.target.textContent)}>
+            Anterior
+          </span>
+        </button>
+        <button>
+          <span onClick={(e) => console.log(e.target.textContent)}>
+            Próximo
+          </span>
+        </button>
+      </div>
+    </div>
+  </>
+)
 
 export { App }
